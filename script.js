7c578159c40660a5c7f29ac46ea0d1ee0aba74d7
abdulloh0512 <abdullohdev0512@gmail.com>
2023-12-08 02:19:32 +0500

    msg

---
+++ script.js
@@ -0,0 +1,127 @@
+// 1-masala
+// function myFunction(a, b) {
+//     return a + b
+// }
+// console.log(myFunction(1, 10));
+
+// 2-masala
+// function myFunction(a, b) {
+//   if (a === b) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// console.log(myFunction('1', '1'));
+
+// 3-masala
+// function myFunction(a) {
+//   return typeof(a)
+// }
+// console.log(myFunction("ss"));
+
+// 4-masala
+// function myFunction(a, n) {
+//   return a[n - 1]
+// }
+
+// console.log(myFunction("salom", 0));
+
+// 5-masala
+// function myFunction(a) {
+//     return a.slice(3)
+// }
+
+// console.log(myFunction("hayr"));
+
+// 6-masala
+// function myFunction(a) {
+//     return a.slice(-3)
+// }
+
+// console.log(myFunction('salom'));
+
+// 7-masala
+// function myFunction(a) {
+//     return a.slice(0, 3)
+// }
+
+// console.log(myFunction("salom"));
+
+// 8-masala
+// function myFunction(a) {
+//   return a.indexOf("is");
+// }
+
+// console.log(myFunction("paris"));
+
+// 9-masala
+// function myFunction(a) {
+//   return a.slice(0, a.length / 2);
+// }
+
+// console.log(myFunction('hello world'));
+
+// 10-masala
+// function myFunction(a) {
+//     return a.slice(0, -3);
+// }
+
+// console.log(myFunction("salom"));
+
+// 11-masala
+// function myFunction(a, b) {
+//   return (b / 100) * a;
+// }
+
+// console.log(myFunction(500, 25));
+
+// 12-masala
+// function myFunction(a, b, c, d, e, f) {
+//     return (((a + b - c) * d) / e) ** f;
+// }
+
+// console.log(myFunction(6,2,1,4,2,3));
+
+// 13-masala
+// function myFunction(a, b) {
+//   return a.indexOf(b) === -1 ? a + b : b + a;
+// }
+
+// console.log(myFunction("salom", "dunyo"));
+
+// 14-masala
+// function myFunction(a){
+//     return a % 2 == 0
+// }
+
+// console.log(myFunction(23));
+
+// 15-masala
+// function myFunction(a, b) {
+//     return b.split(a).length - 1
+// }
+
+// console.log(myFunction("a", "salomaa"));
+
+// 16-masala
+// function myFunction(a) {
+//     return a - Math.floor(a) === 0
+// }
+
+// 17-masala
+// function myFunction(a) {
+//     return a < b ? a / b : a * b
+// }
+
+// 18-masala
+// function myFunction(a) {
+//     return Number(a.toFixed(2));
+// }
+
+// 19-masala
+// function myFunction(a) {
+//   const string = a + "";
+//   const strings = string.split("");
+//   return strings.map((digit) => Number(digit));
+// }
